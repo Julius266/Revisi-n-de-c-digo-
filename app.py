f9bc4a14164ca52cca479f5ab89142911acaaf7f
PC-LAB-AUL-05 AULARIO-LAB-COMPUTACION <PC-LAB-AUL-05@pucesm.edu.ec>
2024-06-05 12:23:14 -0500

    falta comentar y cambiar prestamo

--- app.py
+++ app.py
@@ -7,151 +7,144 @@ app.secret_key = '123456'
 # Configuración de MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'admin123'
-app.config['MYSQL_DB'] = 'biblioteca'
+app.config['MYSQL_DB'] = 'sistema_inventario'
 
 mysql = MySQL(app)
+
 
 # Rutas
 @app.route('/')
 def index():
     cur = mysql.connection.cursor()
-    result_value = cur.execute("SELECT * FROM libros")
+    result_value = cur.execute("SELECT * FROM productos")
     if result_value > 0:
-        books = cur.fetchall()
-        return render_template('index.html', books=books)
+        products = cur.fetchall()
+        return render_template('index.html', products=products)
     return render_template('index.html')
 
 @app.route('/consul_usuario')
 def index_usuario():
     cur = mysql.connection.cursor()
-    result_value = cur.execute("SELECT * FROM usuarios")
+    result_value = cur.execute("SELECT * FROM proveedores")
     if result_value > 0:
-        books = cur.fetchall()
-        return render_template('consulta_usuario.html', books=books)
+        products = cur.fetchall()
+        return render_template('consulta_usuario.html', products=products)
     return render_template('consulta_usuario.html')
 
 @app.route('/add', methods=['GET', 'POST'])
-def add_book():
+def add_product():
     if request.method == 'POST':
-        book_details = request.form
-        title = book_details['title']
-        author = book_details['author']
-        editorial = book_details['editorial']
-        anio = book_details['anio']
-        genero = book_details['genero']
-        stock = book_details['stock']
+        products_details = request.form
+        nombre = products_details['nombre']
+        descripcion = products_details['descripcion']
+        precio = products_details['precio']
+        stock = products_details['stock']
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO libros(titulo, autor, editorial, anio_publicacion,genero,cantidad_disponible) VALUES(%s, %s, %s, %s, %s, %s)", (title, author, editorial, anio, genero, stock))
+        cur.execute("INSERT INTO productos(nombre, descripcion, precio, stock) VALUES(%s, %s, %s, %s)", (nombre, descripcion, precio, stock))
         mysql.connection.commit()
         cur.close()
-        flash('Libro Agregado Satisfactoriamente')
+        flash('Producto Agregado Satisfactoriamente')
         return redirect(url_for('index'))
-    return render_template('libros_add.html')
+    return render_template('products_add.html')
 
 
 @app.route('/add_user', methods=['GET', 'POST'])
 def add_user():
     if request.method == 'POST':
-        book_details = request.form
-        ape = book_details['apellido']
-        nomb = book_details['nombre']
-        correo = book_details['correo']
-        fono = book_details['fono']
+        products_details = request.form
+        nomb = products_details['nombre']
+        contacto = products_details['contacto']
+        telefono = products_details['telefono']
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO usuarios(nombre, apellido, correo, telefono) VALUES(%s, %s, %s, %s)", (nomb, ape, correo, fono))
+        cur.execute("INSERT INTO proveedores(nombre, contacto, telefono) VALUES(%s, %s, %s)", (nomb, contacto, telefono))
         mysql.connection.commit()
         cur.close()
-        flash('Usuario Agregado Satisfactoriamente')
+        flash('Proveedor Agregado Satisfactoriamente')
         return redirect(url_for('index_usuario'))
     return render_template('usuarios_add.html')
 
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit_book(id):
+def edit_product(id):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM libros WHERE id = %s", [id])
-    book = cur.fetchone()
+    cur.execute("SELECT * FROM productos WHERE id = %s", [id])
+    product = cur.fetchone()
     if request.method == 'POST':
-        book_details = request.form
-        title = book_details['title']
-        author = book_details['author']
-        editorial = book_details['editorial']
-        anio = book_details['anio']
-        genero = book_details['genero']
-        stock = book_details['stock']
-        cur.execute("UPDATE libros SET titulo = %s, autor = %s, editorial = %s, anio_publicacion= %s, genero = %s , cantidad_disponible = %s WHERE id = %s", (title, author, editorial, anio, genero, stock, id))
+        products_details = request.form
+        nombre = products_details['nombre']
+        descripcion = products_details['descripcion']
+        precio = products_details['precio']
+        stock = products_details['stock']
+        cur.execute("UPDATE productos SET nombre = %s, descripcion = %s, precio = %s, stock= %s WHERE id = %s", (nombre, descripcion, precio, stock, id))
         mysql.connection.commit()
         cur.close()
-        flash('Libro actualizado satisfactoriamente')
+        flash('Producto actualizado satisfactoriamente')
         return redirect(url_for('index'))
-    return render_template('libros_edit.html', book=book)
+    return render_template('products_edit.html', product=product)
 
 
 @app.route('/edit_user/<int:id>', methods=['GET', 'POST'])
 def edit_user(id):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM usuarios WHERE id = %s", [id])
-    book = cur.fetchone()
+    cur.execute("SELECT * FROM proveedores WHERE id = %s", [id])
+    product = cur.fetchone()
     if request.method == 'POST':
-        book_details = request.form
-        ape = book_details['apellido']
-        nomb = book_details['nombre']
-        correo = book_details['correo']
-        fono = book_details['fono']
+        products_details = request.form
+        nomb = products_details['nombre']
+        contacto = products_details['contacto']
+        telefono = products_details['telefono']
         
-        cur.execute("UPDATE usuarios SET apellido = %s, nombre = %s, correo = %s, telefono= %s WHERE id = %s", (ape, nomb, correo, fono, id))
+        cur.execute("UPDATE proveedores SET nombre = %s, contacto = %s, telefono= %s WHERE id = %s", (nomb, contacto, telefono, id))
         mysql.connection.commit()
         cur.close()
-        flash('Usuario actualizado satisfactoriamente')
+        flash('Proveedor actualizado satisfactoriamente')
         return redirect(url_for('index_usuario'))
-    return render_template('usuarios_edit.html', book=book)
-
+    return render_template('usuarios_edit.html', product=product)
 
 @app.route('/delete/<int:id>', methods=['POST'])
-def delete_book(id):
+def delete_product(id):
     cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM libros WHERE id = %s", [id])
+    cur.execute("DELETE FROM productos WHERE id = %s", [id])
     mysql.connection.commit()
     cur.close()
-    flash('Libro eliminado satisfactoriamente')
+    flash('Producto eliminado satisfactoriamente')
     return redirect(url_for('index'))
 
 
 @app.route('/delete_user/<int:id>', methods=['POST'])
 def delete_user(id):
     cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM usuarios WHERE id = %s", [id])
+    cur.execute("DELETE FROM proovedores WHERE id = %s", [id])
     mysql.connection.commit()
     cur.close()
-    flash('Usuario eliminado satisfactoriamente')
+    flash('Proveedor eliminado satisfactoriamente')
     return redirect(url_for('index_usuario'))
 
 
 
-@app.route('/prestamos')
-def prestamos():
+@app.route('/compras')
+def compras():
     cur = mysql.connection.cursor()
-    cur.execute("SELECT p.id, l.titulo as libro_titulo, u.nombre as prestatario_nombre, p.fecha_prestamo, p.fecha_devolucion "
-                "FROM prestamos p "
-                "JOIN Libros l ON p.id_libro = l.id "
-                "JOIN Usuarios u ON p.id_usuario = u.id")
-    prestamos1 = cur.fetchall()
+    cur.execute("SELECT c.id, p.nombre as nombre_producto, u.nombre as proovedor_nombre, c.fecha, c.cantidad "
+                "FROM Compras c "
+                "JOIN Productos p ON p.id = p.id_product "
+                "JOIN Proovedores u ON p.id = u.id_proovedores")
+    compras1 = cur.fetchall()
 
-    cur.execute("SELECT id, titulo FROM libros")
-    libros1 = cur.fetchall()
+    cur.execute("SELECT id, nombre FROM Productos")
+    producto1 = cur.fetchall()
 
-    cur.execute("SELECT id, concat(apellido,' ', nombre) as nomb FROM usuarios")
+    cur.execute("SELECT id, nombre as nomb FROM Proovedores")
     usuarios1 = cur.fetchall()
 
     cur.close()
-    return render_template('prestamos.html', prestamos=prestamos1, libros=libros1, usuarios=usuarios1)
+    return render_template('compras.html', compras=compras1, productos=producto1, usuarios=usuarios1)
 
 
-@app.route('/prestamos/agregar', methods=['GET', 'POST'])
-def agregar_prestamo():
+@app.route('/compras/agregar', methods=['GET', 'POST'])
+def agregar_compras():
     if request.method == 'POST':
         id_libro = request.form['id_libro']
         id_usuario = request.form['id_usuario']
@@ -162,50 +155,50 @@ def agregar_prestamo():
                 (id_libro, id_usuario, fecha_prestamo))
         mysql.connection.commit()
         cur.close()
-        return redirect(url_for('prestamos'))
+        return redirect(url_for('compras'))
     return render_template('prestamos.html')
 
 
 
-@app.route('/edit_pres/<int:id>', methods=['GET', 'POST'])
-def edit_pres(id):
+@app.route('/edit_compra/<int:id>', methods=['GET', 'POST'])
+def edit_compra(id):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM prestamos WHERE id = %s", [id])
-    book = cur.fetchone()
+    cur.execute("SELECT * FROM compras WHERE id = %s", [id])
+    compra = cur.fetchone()
 
-    cur.execute("SELECT id, titulo FROM libros")
-    libros1 = cur.fetchall()
+    cur.execute("SELECT id, nombre FROM productos")
+    product1 = cur.fetchall()
 
-    cur.execute("SELECT id, concat(apellido,' ', nombre) as nomb FROM usuarios")
+    cur.execute("SELECT id, nombre as nomb FROM proovedores")
     usuarios1 = cur.fetchall()
 
     if request.method == 'POST':
-        book_details = request.form
-        id_libro = book_details['id_libro']
-        id_usuario = book_details['id_usuario']
-        fecha_prestamo = book_details['fecha_prestamo']
-        f_devolucion= book_details['fecha_dev']
+        products_details = request.form
+        id_product = products_details['id_product']
+        id_usuario = products_details['id_proovedor']
+        fecha_compra = products_details['fecha']
+        cantidad = products_details['cantidad']
         
         
-        cur.execute("UPDATE prestamos SET id_libro = %s, id_usuario = %s, fecha_prestamo = %s, fecha_devolucion= %s WHERE id = %s", (id_libro, id_usuario, fecha_prestamo, f_devolucion, id))
+        cur.execute("UPDATE compras SET id_product = %s, id_usuario = %s, fecha_compra = %s, cantidad = %s WHERE id = %s", (id_product, id_usuario, fecha_compra, cantidad, id))
         
         
         
         mysql.connection.commit()
 
         cur.close()
-        flash('Prestamo actualizado satisfactoriamente')
-        return redirect(url_for('prestamos'))
-    return render_template('edit_pres.html', book=book, libros=libros1, usuarios=usuarios1)
+        flash('Compra actualizado satisfactoriamente')
+        return redirect(url_for('compras'))
+    return render_template('edit_pres.html', compra=compra, product=product1, usuarios=usuarios1)
 
 @app.route('/delete_pres/<int:id>', methods=['POST'])
 def delete_pres(id):
     cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM prestamos WHERE id = %s", [id])
+    cur.execute("DELETE FROM compras WHERE id = %s", [id])
     mysql.connection.commit()
     cur.close()
-    flash('Prestamo eliminado satisfactoriamente')
-    return redirect(url_for('prestamos'))
+    flash('Compra eliminado satisfactoriamente')
+    return redirect(url_for('compras'))
 
 if __name__ == '__main__':
     app.run(debug=True)
